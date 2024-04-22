10599598725de3d9ecd31370f6da27f1a47e98d0
Cristian Ramirez <ccrhdbj@outlook.com>
2024-04-22 16:26:44 -0500

    previous

--- src/main.js
+++ src/main.js
@@ -1,1 +1,31 @@
-import './styles/style.css';
+import h from "hyperscript";
+import logo from "./assets/CR5.png";
+import "./styles/style.css";
+
+const app = document.getElementById("app");
+
+const ht = h("header.header", [
+  h(
+    "figure.header--logo-container",
+    h("img.header--logo", {
+      alt: "Ir al inicio",
+      src: logo,
+    })
+  ),
+  h("h2.header--title", [
+    h("span.header--title--name", "Cristian Ramirez"),
+    h("span.header--title--career", "Software Developer"),
+  ]),
+]);
+
+const main = h("main.main-content", [
+  h("ul.main-content--links", [
+    h("li.main-content--link-item", h("a.link-item--a", { href: "/" }), "one"),
+    h("li.main-content--link-item", "two"),
+    h("li.main-content--link-item", "three"),
+    h("li.main-content--link-item", "four"),
+  ]),
+]);
+
+app.appendChild(ht);
+app.appendChild(main);
